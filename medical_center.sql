6c89b705d6e709bafd04eab447a10459337f1ee1
alexm <alex-m@outlook.com>
2023-01-31 18:16:57 -0800

    Add code to create database and 4 tables (standalone and one-to-many relationships)

--- medical_center.sql
+++ medical_center.sql
@@ -1,2 +1,40 @@
 -- Ioana A Mititean
 -- 22.4: SQL Data Modeling Exercise
+
+DROP DATABASE IF EXISTS medical_center;
+
+CREATE DATABASE medical_center;
+
+\c medical_center
+
+CREATE TABLE medcenter
+(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    address TEXT,
+    created DATE
+);
+
+CREATE TABLE doctor
+(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    specialization TEXT,
+    center_id INT REFERENCES medcenter
+);
+
+CREATE TABLE patient
+(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    age INT
+);
+
+CREATE TABLE disease
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT
+);
