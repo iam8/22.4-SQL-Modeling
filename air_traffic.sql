0bd1c5bf9696f83c8a3212bf8462093f40b143e8
alexm <alex-m@outlook.com>
2023-02-01 17:01:26 -0800

    Add minor changes to tickets table columns

--- air_traffic.sql
+++ air_traffic.sql
@@ -10,8 +10,8 @@ CREATE TABLE tickets
 (
   id SERIAL PRIMARY KEY,
   first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL,
-  seat TEXT NOT NULL,
+  last_name TEXT,
+  seat VARCHAR(4) NOT NULL,
   departure TIMESTAMP NOT NULL,
   arrival TIMESTAMP NOT NULL,
   airline TEXT NOT NULL,
